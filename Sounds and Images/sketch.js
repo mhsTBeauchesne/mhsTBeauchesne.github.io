abf43e6d17ea27487afbe1b7a54001bcbdb3ea3d
mhsTBeauchesne <48759778+mhsTBeauchesne@users.noreply.github.com>
2019-04-24 08:55:54 -0400

    Update sketch.js

--- Sounds and Images/sketch.js
+++ Sounds and Images/sketch.js
@@ -1,20 +1,13 @@
-function setup() {
-  // Setup the canvas
-  createCanvas(640, 480);
-}
-
-
-function draw() {
+  var mySound; // Creates a variable to hold the sound
 
   function preload() {
-  mySound = loadSound('babyshark.mp3');  // Loads the sound file into the variable
-}
+    mySound = loadSound('babyshark.mp3');  // Loads the sound file into the variable
+  }
 
-function setup() {
-  // Notice that you have to put the variable name in front of the functions
-  mySound.setVolume(0.1);
-  mySound.play();
-}
-
-
-}
+  function setup() {
+    createCanvas(640, 480);
+    // Notice that you have to put the variable name in front of the functions
+    mySound.setVolume(0.1);
+    mySound.play();
+  }
+  	
