58fb388679e4f9efba1cfa3fd93af9fc24cd052f
mhsTBeauchesne <48759778+mhsTBeauchesne@users.noreply.github.com>
2019-04-25 09:53:54 -0400

    Update sketch.js

--- collisiion test/sketch.js
+++ collisiion test/sketch.js
@@ -48,3 +48,18 @@ ySpeed = ySpeed*(-1);
 }
 
 }
+
+ var mySound; // Creates a variable to hold the sound
+
+  function preload() {
+    mySound = loadSound('babyshark.mp3');  // Loads the sound file into the variable
+  }
+
+  function setup() {
+    createCanvas(640, 480);
+    // Notice that you have to put the variable name in front of the functions
+    mySound.setVolume(0.1);
+    mySound.play();
+  }
+
+
