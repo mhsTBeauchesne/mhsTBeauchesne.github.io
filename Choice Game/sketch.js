567c860a65963c9e6b879ff1995c69b4e97943dd
mhsTBeauchesne <48759778+mhsTBeauchesne@users.noreply.github.com>
2019-05-06 08:48:39 -0400

    Add files via upload

--- Choice Game/sketch.js
+++ Choice Game/sketch.js
@@ -1,203 +1,52 @@
-public class Game {
+var selections = ["rock", "paper","scissors"]
 
-	JFrame window;
-	Container con;
-	JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
-	JLabel titleNameLabel, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
-	Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
-	Font normalFont = new Font("Times New Roman", Font.PLAIN, 28);
-	JButton startButton, choice1, choice2, choice3, choice4;
-	JTextArea mainTextArea;
-	int playerHP, monsterHP, silverRing;
-	String weapon, position;
-
-	TitleScreenHandler tsHandler = new TitleScreenHandler();
-	ChoiceHandler choiceHandler = new ChoiceHandler();
+function setup() {
+  // Setup the canvas
+createCanvas(1200,1600)
+}
 
 
-	public static void main(String[] args) {
+function draw() {
+  //Create the ground
+  //Get user choice
+  var userChoice= prompt('Do you choose rock, paper or scissors?')
+  var computerChoice = random(selections)
 
-		new Game();
-	}
+  print("User Choice",userChoice)
+  print("Computer Choice",computerChoice)
 
-	public Game(){
+  if (userChoice === computerChoice) {
+    background(10,10,10)
+    print("TIE")
+  }
 
-		window = new JFrame();
-		window.setSize(800, 600);
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.getContentPane().setBackground(Color.black);
-		window.setLayout(null);
-		window.setVisible(true);
-		con = window.getContentPane();
+  else if (userChoice==="rock" && computerChoice ==="paper") {
+    background(255,0,0)
+    print("YOU LOSE")
+  }
 
-		titleNamePanel = new JPanel();
-		titleNamePanel.setBounds(100, 100, 600, 150);
-		titleNamePanel.setBackground(Color.black);
-		titleNameLabel = new JLabel("ADVENTURE");
-		titleNameLabel.setForeground(Color.white);
-		titleNameLabel.setFont(titleFont);
+  else if (userChoice==="paper" && computerChoice ===" scissors") {
+    background(255,0,0)
+    print("YOU LOSE")
+  }
 
-		startButtonPanel = new JPanel();
-		startButtonPanel.setBounds(300, 400, 200, 100);
-		startButtonPanel.setBackground(Color.black);
+  else if (userChoice==="scissors" && computerChoice ==="rock") {
+    background(255,0,0)
+    print("YOU LOSE")
+  }
 
-		startButton = new JButton("START");
-		startButton.setBackground(Color.black);
-		startButton.setForeground(Color.white);
-		startButton.setFont(normalFont);
-		startButton.addActionListener(tsHandler);
-		startButton.setFocusPainted(false);
+  else if (userChoice==="rock" && computerChoice ==="scissors") {
+    background(0,255,0)
+    print("YOU WON")
+  }
 
-		titleNamePanel.add(titleNameLabel);
-		startButtonPanel.add(startButton);
+  else if (userChoice==="scissors" && computerChoice ==="paper") {
+    background(0,255,0)
+    print("YOU WON")
+  }
 
-		con.add(titleNamePanel);
-		con.add(startButtonPanel);
-	}
-
-	public void createGameScreen(){
-
-		titleNamePanel.setVisible(false);
-		startButtonPanel.setVisible(false);
-
-		mainTextPanel = new JPanel();
-		mainTextPanel.setBounds(100, 100, 600, 250);
-		mainTextPanel.setBackground(Color.black);
-		con.add(mainTextPanel);
-
-		mainTextArea = new JTextArea("This is the main text are. This game is going to be great. I'm sure of it!!!!!!!");
-		mainTextArea.setBounds(100, 100, 600, 250);
-		mainTextArea.setBackground(Color.black);
-		mainTextArea.setForeground(Color.white);
-		mainTextArea.setFont(normalFont);
-		mainTextArea.setLineWrap(true);
-		mainTextPanel.add(mainTextArea);
-
-		choiceButtonPanel = new JPanel();
-		choiceButtonPanel.setBounds(250, 350, 300, 150);
-		choiceButtonPanel.setBackground(Color.black);
-		choiceButtonPanel.setLayout(new GridLayout(4,1));
-		con.add(choiceButtonPanel);
-
-		choice1 = new JButton("Choice 1");
-		choice1.setBackground(Color.black);
-		choice1.setForeground(Color.white);
-		choice1.setFont(normalFont);
-		choice1.setFocusPainted(false);
-		choice1.addActionListener(choiceHandler);
-		choice1.setActionCommand("c1");
-		choiceButtonPanel.add(choice1);
-		choice2 = new JButton("Choice 2");
-		choice2.setBackground(Color.black);
-		choice2.setForeground(Color.white);
-		choice2.setFont(normalFont);
-		choice2.setFocusPainted(false);
-		choice2.addActionListener(choiceHandler);
-		choice2.setActionCommand("c2");
-		choiceButtonPanel.add(choice2);
-		choice3 = new JButton("Choice 3");
-		choice3.setBackground(Color.black);
-		choice3.setForeground(Color.white);
-		choice3.setFont(normalFont);
-		choice3.setFocusPainted(false);
-		choice3.addActionListener(choiceHandler);
-		choice3.setActionCommand("c3");
-		choiceButtonPanel.add(choice3);
-		choice4 = new JButton("Choice 4");
-		choice4.setBackground(Color.black);
-		choice4.setForeground(Color.white);
-		choice4.setFont(normalFont);
-		choice4.setFocusPainted(false);
-		choice4.addActionListener(choiceHandler);
-		choice4.setActionCommand("c4");
-		choiceButtonPanel.add(choice4);
-
-		playerPanel = new JPanel();
-		playerPanel.setBounds(100, 15, 600, 50);
-		playerPanel.setBackground(Color.black);
-		playerPanel.setLayout(new GridLayout(1,4));
-		con.add(playerPanel);
-		hpLabel = new JLabel("HP:");
-		hpLabel.setFont(normalFont);
-		hpLabel.setForeground(Color.white);
-		playerPanel.add(hpLabel);
-		hpLabelNumber = new JLabel();
-		hpLabelNumber.setFont(normalFont);
-		hpLabelNumber.setForeground(Color.white);
-		playerPanel.add(hpLabelNumber);
-		weaponLabel = new JLabel("Weapon:");
-		weaponLabel.setFont(normalFont);
-		weaponLabel.setForeground(Color.white);
-		playerPanel.add(weaponLabel);
-		weaponLabelName = new JLabel();
-		weaponLabelName.setFont(normalFont);
-		weaponLabelName.setForeground(Color.white);
-		playerPanel.add(weaponLabelName);
-
-		playerSetup();
-
-	}
-	public void playerSetup(){
-		playerHP = 15;
-		monsterHP = 20;
-		weapon = "Knife";
-		weaponLabelName.setText(weapon);
-		hpLabelNumber.setText("" + playerHP);
-
-		townGate();
-	}
-	public void townGate(){
-		position = "townGate";
-		mainTextArea.setText("You are at the gate of the town. \nA guard is standing in front of you. \n\nWhat do you do?");
-		choice1.setText("Talk to the guard");
-		choice2.setText("Attack the guard");
-		choice3.setText("Leave");
-		choice4.setText("");
-	}
-	public void talkGuard(){
-		position = "talkGuard";
-		mainTextArea.setText("Guard: Hello stranger. I have never seen your face. \nI'm sorry but we cannot let a stranger enter our town.");
-		choice1.setText(">");
-		choice2.setText("");
-		choice3.setText("");
-		choice4.setText("");
-	}
-	public void attackGuard(){
-		position = "attackGuard";
-		mainTextArea.setText("Guard: Hey don't be stupid!\n\nThe guard fought back and hit you hard.\n(You receive 3 damage)");
-		playerHP = playerHP -3;
-		hpLabelNumber.setText(""+playerHP);
-		choice1.setText(">");
-		choice2.setText("");
-		choice3.setText("");
-		choice4.setText("");
-	}
-	public void crossRoad(){
-		position = "crossRoad";
-		mainTextArea.setText("You are at a crossroad.\nIf you go south, you will go back to the town.");
-		choice1.setText("Go north");
-		choice2.setText("Go east");
-		choice3.setText("Go south");
-		choice4.setText("Go west");
-	}
-	public void north(){
-		position = "north";
-		mainTextArea.setText("There is a river. \nYou drink the water and rest at the riverside. \n\n(Your HP is recovered by 2)");
-		playerHP = playerHP + 2;
-		hpLabelNumber.setText(""+playerHP);
-		choice1.setText("Go south");
-		choice2.setText("");
-		choice3.setText("");
-		choice4.setText("");
-	}
-	public void east(){
-		position = "east";
-		mainTextArea.setText("You walked into a forest and found a Long Sword!\n\n(You obtained a Long Sword)");
-		weapon = "Long Sword";
-		weaponLabelName.setText(weapon);
-		choice1.setText("Go west");
-		choice2.setText("");
-		choice3.setText("");
-		choice4.setText("");
-
-	}
+  else if (userChoice==="paper" && computerChoice ==="rock") {
+    background(0,255,0)
+    print("YOU WON")
+  }
+}
