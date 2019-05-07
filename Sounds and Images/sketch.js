0451d54e133fdd3854172a4ee83289d565a5b302
mhsTBeauchesne <48759778+mhsTBeauchesne@users.noreply.github.com>
2019-05-07 10:27:03 -0400

    Add files via upload

--- Sounds and Images/sketch.js
+++ Sounds and Images/sketch.js
@@ -1,14 +1,21 @@
 
 let song;
+let bg;
 
 function preload() {
-  song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
+  song = loadSound('EXPLOSION SOUND EFFECT.mp3');
+  bg = loadImage('530916-red-dead-redemption-2-for-playstation-4.png')
+
 }
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(1300,1000);
   song.loop(); // song is ready to play during setup() because it was loaded during preload
-  background(0, 255, 0);
+  background(bg);
+
+}
+function draw(){
+image(bg,0,0)
 }
 
 function mousePressed() {
